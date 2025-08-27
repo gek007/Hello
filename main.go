9d8b48f3f7aa59232d2ae28b7be2b966f46fa53f
kostya shilkrot <kostya.shilkrot@gmail.com>
2025-08-27 10:12:20 +0300

    new types

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
-	fmt.Println("Kostya!")
-	fmt.Println("Shilkro000t!")
+	fmt.Printf("Kostya!")
+	fmt.Printf("Shilkro000t!")
+
+	userHeight := 1.68
+	userKg := 92.0
+	var IMT = userKg / math.Pow(userHeight, 2)
+	fmt.Printf("Ваш индекс массы тела: %.2f\n", IMT)
+
 }
